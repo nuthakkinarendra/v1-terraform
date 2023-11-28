06c333c8c3318194ebfae90f722652ba7d61fb04
nuthakkinarendra <70637778+nuthakkinarendra@users.noreply.github.com>
2023-11-28 23:39:45 +0530

    Create wave-cafe.sh

---
+++ wave-cafe.sh
@@ -0,0 +1,5 @@
+sudo yum update
+sudo yum install httpd -y
+wget https://www.free-css.com/assets/files/free-css-templates/download/page290/wave-cafe.zip
+sudo unzip wave-cafe.zip
+sudo mv 2121_wave_cafe /var/www/html/ 
